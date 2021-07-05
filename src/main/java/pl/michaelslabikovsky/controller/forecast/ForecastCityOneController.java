6e9cd83101b679cc28cf9f4451744d43387503f7
suabicii <michal.slabik3@wp.pl>
2021-07-05 14:51:47 +0200

    Added fx:ids for weather data

--- src/main/java/pl/michaelslabikovsky/controller/forecast/ForecastCityOneController.java
+++ src/main/java/pl/michaelslabikovsky/controller/forecast/ForecastCityOneController.java
@@ -1,6 +1,50 @@
 package pl.michaelslabikovsky.controller.forecast;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class ForecastCityOneController extends ForecastController {
+
+    @FXML
+    private Label firstDayResultCityOne;
+
+    @FXML
+    private Label firstDayTemperatureCityOne;
+
+    @FXML
+    private Label pressureCityOne;
+
+    @FXML
+    private Label windSpeedCityOne;
+
+    @FXML
+    private Label humidityCityOne;
+
+    @FXML
+    private Label secondDayResultCityOne;
+
+    @FXML
+    private Label secondDayTemperatureOne;
+
+    @FXML
+    private Label thirdDayResultCityOne;
+
+    @FXML
+    private Label thirdDayTemperatureCityOne;
+
+    @FXML
+    private Label fourthDayResultCityOne;
+
+    @FXML
+    private Label fourthDayTemperatureCityOne;
+
+    @FXML
+    private Label fifthDayResultCityOne;
+
+    @FXML
+    private Label fifthDayTemperatureCityOne;
+
+    /** Icons */
 
     @Override
     public void getForecast() {
--- src/main/java/pl/michaelslabikovsky/controller/forecast/ForecastCityTwoController.java
+++ src/main/java/pl/michaelslabikovsky/controller/forecast/ForecastCityTwoController.java
@@ -1,6 +1,50 @@
 package pl.michaelslabikovsky.controller.forecast;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class ForecastCityTwoController extends ForecastController {
+
+    @FXML
+    private Label firstDayResultCityTwo;
+
+    @FXML
+    private Label firstDayTemperatureCityTwo;
+
+    @FXML
+    private Label pressureCityTwo;
+
+    @FXML
+    private Label windSpeedCityTwo;
+
+    @FXML
+    private Label humidityCityTwo;
+
+    @FXML
+    private Label secondDayResultCityTwo;
+
+    @FXML
+    private Label secondDayTemperatureTwo;
+
+    @FXML
+    private Label thirdDayResultCityTwo;
+
+    @FXML
+    private Label thirdDayTemperatureCityTwo;
+
+    @FXML
+    private Label fourthDayResultCityTwo;
+
+    @FXML
+    private Label fourthDayTemperatureCityTwo;
+
+    @FXML
+    private Label fifthDayResultCityTwo;
+
+    @FXML
+    private Label fifthDayTemperatureCityTwo;
+
+    /** Icons */
 
     @Override
     public void getForecast() {
