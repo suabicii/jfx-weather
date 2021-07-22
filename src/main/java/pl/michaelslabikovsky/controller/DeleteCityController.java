b86032b98457225364b970828d13ea7ead5106d2
suabicii <michal.slabik3@wp.pl>
2021-07-22 12:31:27 +0200

    Added displaying locations list in deleting window

--- src/main/java/pl/michaelslabikovsky/controller/DeleteCityController.java
+++ src/main/java/pl/michaelslabikovsky/controller/DeleteCityController.java
@@ -5,30 +5,52 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
-import pl.michaelslabikovsky.model.Location;
 import pl.michaelslabikovsky.model.LocationsDBModel;
+import pl.michaelslabikovsky.model.SavedLocation;
 import pl.michaelslabikovsky.view.ViewFactory;
 
 import java.net.URL;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class DeleteCityController extends BaseController implements Initializable {
 
     @FXML
-    private TableView<Location> locationTable;
+    private TableView<SavedLocation> locationTable;
 
     @FXML
-    private TableColumn<Location, String> nameCol;
+    private TableColumn<SavedLocation, String> nameCol;
 
     @FXML
     private Label messageLabel;
 
     private LocationsDBModel locationsDBModel;
+    private SavedLocation savedLocation;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        locationsDBModel = new LocationsDBModel();
+        List<String> locationsList = null;
+
+        try {
+            locationsList = new ArrayList<>(locationsDBModel.selectAllFromDB());
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+
+        nameCol.setCellValueFactory(new PropertyValueFactory<SavedLocation, String>("name"));
+        locationTable.getColumns().add(nameCol);
         locationTable.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+
+        for (int i = 0; i < locationsList.size(); i++) {
+            savedLocation = new SavedLocation(locationsList.get(i));
+            locationTable.getItems().add(savedLocation);
+        }
     }
 
     public DeleteCityController(ViewFactory viewFactory, String fxmlName) {
---
+++ src/main/java/pl/michaelslabikovsky/model/SavedLocation.java
@@ -0,0 +1,14 @@
+package pl.michaelslabikovsky.model;
+
+public class SavedLocation {
+
+    private String name;
+
+    public SavedLocation(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
