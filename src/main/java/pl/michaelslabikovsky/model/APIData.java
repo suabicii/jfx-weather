dbe00470410eb21671d8a8dee17d26b7569fce6e
suabicii <michal.slabik3@wp.pl>
2021-08-05 18:03:51 +0200

    Fixed string encoding

--- src/main/java/pl/michaelslabikovsky/model/APIData.java
+++ src/main/java/pl/michaelslabikovsky/model/APIData.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 public abstract class APIData {
@@ -22,10 +23,11 @@ public abstract class APIData {
 
     protected void connectToAPI(String cityName, String mainAPIPart, String additionalAPIPart) throws MalformedURLException {
         this.url = new URL(mainAPIPart + cityName + "&appid=" + API_KEY + additionalAPIPart);
-        HttpURLConnection conn = null;
+        HttpURLConnection conn;
         try {
             conn = (HttpURLConnection)url.openConnection();
             conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json;charset=UTF-8");
             conn.connect();
             this.responseCode = conn.getResponseCode();
         } catch (IOException e) {
@@ -38,7 +40,13 @@ public abstract class APIData {
             throw new RuntimeException("HttpResponseCode: " + responseCode);
         } else {
             Scanner sc = new Scanner(url.openStream());
-            while (sc.hasNext()) result += sc.nextLine();
+            while (sc.hasNext()) {
+                String line = sc.nextLine();
+                byte[] bytes = line.getBytes();
+
+                String utf8Bytes = new String(bytes, StandardCharsets.UTF_8);
+                this.result += utf8Bytes;
+            }
             sc.close();
             return result;
         }
