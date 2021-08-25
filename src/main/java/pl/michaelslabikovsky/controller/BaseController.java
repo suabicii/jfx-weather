7c76a1ed9026f43ee1bb5dc0c840868e1b8c0d7c
suabicii <michal.slabik3@wp.pl>
2021-08-25 20:29:20 +0200

    Made fxmlName field final

--- src/main/java/pl/michaelslabikovsky/controller/BaseController.java
+++ src/main/java/pl/michaelslabikovsky/controller/BaseController.java
@@ -7,7 +7,7 @@ import pl.michaelslabikovsky.view.ViewFactory;
 public abstract class BaseController {
 
     protected ViewFactory viewFactory;
-    private String fxmlName;
+    private final String fxmlName;
 
     public BaseController(ViewFactory viewFactory, String fxmlName) {
         this.viewFactory = viewFactory;
