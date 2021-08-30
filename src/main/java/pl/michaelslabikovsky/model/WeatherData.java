a8aac32e80b1c357430a74faf1851865b54cddd6
suabicii <michal.slabik3@wp.pl>
2021-08-30 17:09:45 +0200

    Optimised imports

--- src/main/java/pl/michaelslabikovsky/model/WeatherData.java
+++ src/main/java/pl/michaelslabikovsky/model/WeatherData.java
@@ -1,7 +1,5 @@
 package pl.michaelslabikovsky.model;
 
-import java.net.MalformedURLException;
-
 public class WeatherData extends ApiData {
 
     private final static String MAIN_API_PART = "https://api.openweathermap.org/data/2.5/forecast?q=";
