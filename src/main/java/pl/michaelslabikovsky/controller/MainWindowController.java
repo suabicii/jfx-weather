7b598a9ab4ad6ff87e7300e49efcd8f86b2c460c
suabicii <michal.slabik3@wp.pl>
2021-09-02 12:05:29 +0200

    Optimised imports

--- src/main/java/pl/michaelslabikovsky/controller/MainWindowController.java
+++ src/main/java/pl/michaelslabikovsky/controller/MainWindowController.java
@@ -5,7 +5,6 @@ import javafx.concurrent.Service;
 import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressIndicator;
