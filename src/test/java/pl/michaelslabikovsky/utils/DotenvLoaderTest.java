4af8246eac7362c499bfbca4870ba1efbbf34916
suabicii <michal.slabik3@wp.pl>
2021-09-02 17:32:39 +0200

    Refactored DotenvLoaderTest

--- src/test/java/pl/michaelslabikovsky/utils/DotenvLoaderTest.java
+++ src/test/java/pl/michaelslabikovsky/utils/DotenvLoaderTest.java
@@ -13,12 +13,12 @@ import static org.hamcrest.Matchers.is;
 
 class DotenvLoaderTest {
 
-    private final String tempFileName = ".env-temp";
-    private final String tempKey = "a4df15c3943fe91408e1436c4fad4208ceb29b02"; //przerobione słowo „ziemniak” xD
+    private final static String TEMP_FILE_NAME = ".env-temp";
+    private final static String TEMP_KEY = "a4df15c3943fe91408e1436c4fad4208ceb29b02"; //przerobione słowo „ziemniak” xD
 
     @AfterEach
     void deleteTemporaryFile() {
-        File file = new File(tempFileName);
+        File file = new File(TEMP_FILE_NAME);
         file.deleteOnExit();
     }
 
@@ -26,21 +26,21 @@ class DotenvLoaderTest {
     void shouldReturnEnvVariable() {
         //given
         writeTemporaryEnvFile();
-        DotenvLoader dotenvLoader = new DotenvLoader(tempFileName);
+        DotenvLoader dotenvLoader = new DotenvLoader(TEMP_FILE_NAME);
 
         //when
         dotenvLoader.loadEnvFile();
         String generatedKey = dotenvLoader.getEnvVariable("TEMP_KEY");
 
         //then
-        assertThat(generatedKey, is(tempKey));
+        assertThat(generatedKey, is(TEMP_KEY));
     }
 
     private void writeTemporaryEnvFile() {
         try {
-            FileWriter fileWriter = new FileWriter(tempFileName);
+            FileWriter fileWriter = new FileWriter(TEMP_FILE_NAME);
             BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-            bufferedWriter.write("TEMP_KEY=" + tempKey);
+            bufferedWriter.write("TEMP_KEY=" + TEMP_KEY);
             bufferedWriter.close();
         } catch (IOException e) {
             e.printStackTrace();
