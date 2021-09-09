ba2a47b700d0f70fa5ec200abcc63ace13533517
suabicii <michal.slabik3@wp.pl>
2021-09-09 17:02:28 +0200

    Deleted dependency duplications

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,18 +6,10 @@ module pl.michaelslabikovsky {
     requires org.json;
     requires java.sql;
     requires sqlite.jdbc;
-    requires org.junit.jupiter.api;
-    requires org.junit.jupiter.engine;
-    requires org.junit.platform.commons;
-    requires org.junit.platform.launcher;
-    requires org.junit.platform.engine;
-    requires org.hamcrest;
 
     opens pl.michaelslabikovsky to javafx.fxml;
     opens pl.michaelslabikovsky.controller to javafx.fxml;
     opens pl.michaelslabikovsky.controller.currentweather to javafx.fxml;
     opens pl.michaelslabikovsky.controller.forecast to javafx.fxml;
-    opens pl.michaelslabikovsky.model to javafx.base, org.junit.platform.commons;
-    opens pl.michaelslabikovsky.utils to org.junit.platform.commons;
     exports pl.michaelslabikovsky;
 }
