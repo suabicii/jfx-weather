bbdb8b797d582bf7556d0a37b3986f209950e6e3
suabicii <michal.slabik3@wp.pl>
2021-09-23 14:45:44 +0200

    Create class for testing LocationsDBModel

---
+++ src/test/java/pl/michaelslabikovsky/model/LocationsDBModelTest.java
@@ -0,0 +1,25 @@
+package pl.michaelslabikovsky.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LocationsDBModelTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void insertIntoTable() {
+    }
+
+    @Test
+    void selectAllFromDB() {
+    }
+
+    @Test
+    void deleteFromTable() {
+    }
+}
