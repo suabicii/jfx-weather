db841e292aa2048f0a963b6f96c213f15ca0c7c3
suabicii <michal.slabik3@wp.pl>
2021-09-27 14:14:04 +0200

    Created test class for LocationClient

---
+++ src/test/java/pl/michaelslabikovsky/model/LocationClientTest.java
@@ -0,0 +1,16 @@
+package pl.michaelslabikovsky.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LocationClientTest {
+
+    @Test
+    void loadLocationData() {
+    }
+
+    @Test
+    void getFoundLocation() {
+    }
+}
